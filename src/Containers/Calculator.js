f7ecea805a627374646544964efd88c5187bd397
DavidSandor <sandor.david89@gmail.com>
2019-06-27 21:18:32 +0200

    Calculator class

---
+++ src/Containers/Calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    state = {
+        operationChain: [],
+        result: null
+    }
+
+    render(){
+        return <div></div>
+    }
+}
+
+export default Calculator;
