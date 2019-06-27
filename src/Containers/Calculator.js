1f8153576517a515d027152166a54a67e33e2c2f
DavidSandor <sandor.david89@gmail.com>
2019-06-27 22:10:39 +0200

    Calculator layout update

--- src/Containers/Calculator.js
+++ src/Containers/Calculator.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import './Calculator.scss';
 import Display from '../Components/Display';
+import Button from '../Components/Button';
+
 
 class Calculator extends Component {
     state = {
         operationChain: [],
-        result: null
+        result: 0
     }
 
     render(){
         return <div className='Calculator'>
-            <Display result='5' operations='2+3'></Display>
+            <Display result={this.state.result} operations='2+3'></Display>
+            <div className='FlexRow'>
+                <Button content='7' ></Button>
+                <Button content='8' ></Button>
+                <Button content='9' ></Button>
+                <Button content='/' ></Button>
+            </div>
+            <div className='FlexRow'>
+                <Button content='4' ></Button>
+                <Button content='5' ></Button>
+                <Button content='6' ></Button>
+                <Button content='x' ></Button>
+            </div>
+            <div className='FlexRow'>
+                <Button content='1' ></Button>
+                <Button content='2' ></Button>
+                <Button content='3' ></Button>
+                <Button content='-' ></Button>
+            </div>
+            <div className='FlexRow'>
+                <Button content='0' ></Button>
+                <Button content='.' ></Button>
+                <Button content='CLR' ></Button>
+                <Button content='+' ></Button>
+            </div>
         </div>
     }
 }
